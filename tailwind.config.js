436df245dccd269a05d3a8beaa528480a424b303
DillonAdriyan <dillon.adrian00@gmail.com>
2024-06-04 21:43:23 +0700

    tambah detail product

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "/data/data/com.termux/files/home/node_modules/flowbite/**/*.js"],
+  content: ["./*.html", "/data/data/com.termux/files/home/node_modules/flowbite/**/*.js"],
   theme: {
    fontFamily : {
     'head': ['"Poppins', 'sans-serif']
@@ -16,6 +16,19 @@ module.exports = {
      },
     },
   },
-  plugins: [require('/data/data/com.termux/files/home/node_modules/flowbite/plugin')],
+  plugins: [
+   require('/data/data/com.termux/files/home/node_modules/flowbite/plugin'),
+   require('/data/data/com.termux/files/home/node_modules/daisyui'),
+   ],
+   daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: false, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
 
